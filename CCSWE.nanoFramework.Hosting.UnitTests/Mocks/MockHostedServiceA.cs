7480ec8ac7d04ccbde73d2bcf8eb07b12d109c91
Cory Charlton <ccharlton@pobox.com>
2024-04-23 23:19:46 -0700

    Adding unit tests

---
+++ CCSWE.nanoFramework.Hosting.UnitTests/Mocks/MockHostedServiceA.cs
@@ -0,0 +1,6 @@
+﻿namespace CCSWE.nanoFramework.Hosting.UnitTests.Mocks
+{
+    public class MockHostedServiceA: MockHostedService
+    {
+    }
+}
---
+++ CCSWE.nanoFramework.Hosting.UnitTests/Mocks/MockServiceA.cs
@@ -0,0 +1,9 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+namespace CCSWE.nanoFramework.Hosting.UnitTests.Mocks
+{
+    public class MockServiceA { }
+}
